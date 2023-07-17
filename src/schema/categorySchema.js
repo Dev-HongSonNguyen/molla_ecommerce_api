05def3894d5ccdc087e381029436a37a29715480
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-18 02:07:35 +0700

    be/update-joi-message

--- src/schema/categorySchema.js
+++ src/schema/categorySchema.js
@@ -1,6 +1,8 @@
 import Joi from "joi";
 const categoryValidate = Joi.object({
   _id: Joi.string(),
-  name: Joi.string().required().trim(),
+  name: Joi.string().required().trim().messages({
+    "string.empty": "Vui lòng nhập vào tên danh mục sản phẩm",
+  }),
 });
 export default categoryValidate;
