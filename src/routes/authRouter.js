adbf102136faf679b07d7f3af6a67d0e39b921f0
nguyenhongson <nguyenhongson20433@gmail.com>
2023-08-03 01:39:00 +0700

    be/api-updateUser-deleteUser

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,13 +1,19 @@
 import express from "express";
 import {
+  deleteUser,
   getAllUsers,
   getOneUser,
   signin,
   signup,
+  updateUser,
 } from "../controller/authController";
+import { authenticate } from "../middleware/authenticate";
+import { authorization } from "../middleware/authorization";
 const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/users", getAllUsers);
 router.get("/users/:id", getOneUser);
+router.put("/users/:id", authenticate, authorization, updateUser);
+router.delete("/users/:id", authenticate, authorization, deleteUser);
 export default router;
